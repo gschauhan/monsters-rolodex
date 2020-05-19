0f2073cab707c137e1b0d3b6a43dae905d799c43
gschauhan <gaurav.chauhan36@gmail.com>
2020-05-19 12:30:15 +0530

    Updates

---
+++ precache-manifest.2fe98b6131ad6f36b491260a1a05f9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bf74468dc9f76935b99c6400b1587d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0300bca4e4c94bac4799",
+    "url": "/monsters-rolodex/static/css/main.0ba27130.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0300bca4e4c94bac4799",
+    "url": "/monsters-rolodex/static/js/main.caf66c47.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
